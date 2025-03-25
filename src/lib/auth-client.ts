d41c2bbb4fb11cdc2edb56c5c0b5ab5239347cf1
SyntaxLexx <spencermainaa@gmail.com>
2025-03-25 23:18:27 +0100

    Big Bang

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,8 @@
+import { createAuthClient } from "better-auth/react";
+
+const authClient = createAuthClient({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  plugins: [],
+});
+
+export default authClient;
